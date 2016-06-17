de349c595447671fd365f4c00570699a176ae06c
Sean Mc Allister <sean@gmx.org>
2016-06-17 13:38:02 +0200

    nicer locate me button

--- frontend/js/components/locationControl.js
+++ frontend/js/components/locationControl.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 import L from 'leaflet';
 import { MapControl } from 'react-leaflet';
 
-export default class LocationControl extends MapControl {  // note we're extending MapControl from react-leaflet, not Component from react
-
-  locateMe() {
-    console.log('Locate!',arguments);
-  }
+export default class LocationControl extends MapControl {
 
   componentWillMount() {
-    const locationControl = L.control({position: 'topleft'});  // see http://leafletjs.com/reference.html#control-positions for other positions
+    const locationControl = L.control({position: 'topleft'});
     const jsx = (
-      <a href='#' onClick={this.props.locate}>
-        Locate Me!
-      </a>
+      <div className='leaflet-bar'>
+        <a href='#' onClick={this.props.locate}>
+          <i className='fa fa-crosshairs'></i>
+        </a>
+      </div>
     );
 
     locationControl.onAdd = function (map) {
