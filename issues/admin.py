67b1ecda340cad1d4ba2e10def37b6ed3c75974b
Sean Mc Allister <sean@gmx.org>
2016-09-22 10:50:14 +0200

    filter issues by site and status in admin

--- issues/admin.py
+++ issues/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from .models import Issue, IssueType, IssueComment, IssueStatus
 
 class IssueAdmin(admin.ModelAdmin):
-    list_display = ('title', 'issue_type', 'created_by', 'current_status', )
+    list_display = ('title', 'issue_type', 'created_by', 'current_status', 'site')
+    list_filter = ('site', 'current_status')
 
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(IssueType)
