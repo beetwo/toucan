06922a15473674f9ed83844d5f1a4f53f14cf4f6
Sean Mc Allister <sean@gmx.org>
2016-11-29 13:11:17 +0100

    added some fixtures

--- issue_tracker/settings/__init__.py
+++ issue_tracker/settings/__init__.py
@@ -159,6 +159,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets')
 ]
 
+FIXTURE_DIRS = [
+    os.path.join(BASE_DIR, 'fixtures/')
+]
+
+
 LOGIN_REDIRECT_URL = 'user_profile:personal_profile'
 
 CHANNEL_LAYERS = {
