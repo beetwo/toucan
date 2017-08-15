27658227a21166d50e0142d5793643eb7bde146d
Thomas Fadrus <thomas@fadr.at>
2017-08-15 11:09:38 +0200

    Add fullscreen support for filter on small screens

--- frontend/js/components/issueList.js
+++ frontend/js/components/issueList.js
@@ -111,8 +111,25 @@ class IssueFilter extends React.Component {
           </div>
         </div>
         </div>
-        <div className="filter collapse" id="issueFilter">
-            {items}
+        <div className="filter collapse fullscreen-sm" id="issueFilter">
+            <div className="fullscreen-header flex-container">
+              <div className="flex-col">
+                <a href="#" className="fullscreen-close" data-toggle="collapse" data-target="#issueFilter">
+                  <span className="icon icon-close"></span> Filter
+                </a>
+              </div>
+              <div className="flex-col text-right">
+                <a href="#">
+                  Reset
+                </a>
+              </div>
+            </div>
+            <div className="fullscreen-content">
+              {items}
+            </div>
+            <div className="fullscreen-footer">
+              <button className="btn btn-primary btn-block" data-toggle="collapse" data-target="#issueFilter">Show results</button>
+            </div>
         </div>
       </div>
     );
@@ -176,112 +193,6 @@ class IssueListUI extends React.Component {
             <div className="issue-list-form">
             <IssueFilter {...this.props} filterOptions={this.props.filterOptions}/>
 
-              <div className="filter collapse" id="issueFilter">
-                <div className="filter-section">
-                  <div className="filter-head flex-container flex-vCenter">
-                    <div className="flex-col">
-                    <h5 className="filter-heading">
-                      Categories
-                    </h5>
-                    </div>
-                    <div className="flex-col text-right">
-                      <a className="filter-toggle" href="#" data-toggle="collapse" data-target="#issueFilterCategories">Hide categories <span className="icon icon-chevron"></span></a>
-                    </div>
-                  </div>
-                  <div className="filter-body collapse in" id="issueFilterCategories">
-                    <div className="filter-item">
-                      <div className="filter-check text-center">
-                        <input type="checkbox"></input>
-                      </div>
-                      <div className="filter-title">
-                        <span className="filter-icon icon icon-lg icon-core-relief-item"></span>
-                        Core Relief Items
-                      </div>
-                    </div>
-                    <div className="filter-item">
-                      <div className="filter-check text-center">
-                        <input type="checkbox"></input>
-                      </div>
-                      <div className="filter-title">
-                        <span className="filter-icon icon icon-lg icon-shelter"></span>
-                        Shelter
-                      </div>
-                    </div>
-                    <div className="filter-item">
-                      <div className="filter-check text-center">
-                        <input type="checkbox"></input>
-                      </div>
-                      <div className="filter-title">
-                        <span className="filter-icon icon icon-lg icon-health"></span>
-                        Health
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="filter-section">
-                  <div className="filter-head flex-container flex-vCenter">
-                    <div className="flex-col">
-                    <h5 className="filter-heading">
-                      Status
-                    </h5>
-                    </div>
-                    <div className="flex-col text-right">
-                      <a className="filter-toggle" href="#" data-toggle="collapse" data-target="#issueFilterStatus">Hide status <span className="icon icon-chevron"></span></a>
-                    </div>
-                  </div>
-                  <div className="filter-body collapse in" id="issueFilterStatus">
-                    <div className="filter-item">
-                      <div className="filter-check text-center">
-                        <input type="checkbox"></input>
-                      </div>
-                      <div className="filter-title">
-                        Open
-                      </div>
-                    </div>
-                    <div className="filter-item">
-                      <div className="filter-check text-center">
-                        <input type="checkbox"></input>
-                      </div>
-                      <div className="filter-title">
-                        In Progress
-                      </div>
-                    </div>
-                    <div className="filter-item">
-                      <div className="filter-check text-center">
-                        <input type="checkbox"></input>
-                      </div>
-                      <div className="filter-title">
-                        Resolved
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="filter-section">
-                  <div className="filter-head flex-container flex-vCenter">
-                    <div className="flex-col">
-                    <h5 className="filter-heading">
-                      Organisations
-                    </h5>
-                    </div>
-                    <div className="flex-col text-right">
-                      <a className="filter-toggle" href="#" data-toggle="collapse" data-target="#issueFilterOrg">More <span className="icon icon-chevron"></span></a>
-                    </div>
-                  </div>
-                  <div className="filter-body collapse" id="issueFilterOrg">
-                    <div className="filter-item">
-                      <div className="filter-check text-center">
-                        <input type="checkbox"></input>
-                      </div>
-                      <div className="filter-title">
-                        Fadrat
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="filter-foot">
-                <button className="btn btn-info btn-block btn-sm">See results</button>
-                </div>
-              </div>
             </div>
             <ScrollContainer scrollKey='toucan-issue-list'>
             {/* the actual list of issues */}
