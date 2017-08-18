7e14f5ca6c4faa33685e02a69c2a9902280df369
Sean Mc Allister <sean.mcallister@apa.at>
2017-08-18 13:46:39 +0200

    move icons and backgrund image urls for webpack

--- frontend/js/components/map/markers.js
+++ frontend/js/components/map/markers.js
@@ -1,27 +1,14 @@
-import Leaflet from "leaflet";
+import { divIcon } from "leaflet";
 import { Marker } from "react-leaflet";
 import { getIconClassForIssueType } from "../icons/issueType";
 
-require("Leaflet.extra-markers/src/assets/css/leaflet.extra-markers.css");
-let extraMarkers = require("Leaflet.extra-markers/src/assets/js/leaflet.extra-markers");
-
 function getMarkerForIssue(issue = {}, opts = {}) {
   let issue_type = issue.issue_types[0];
-  const defaulIconProps = {
-    icon: getIconClassForIssueType(issue_type, 'icon-w-'),
-    // marker colors
-    //'red', 'orange-dark', 'orange', 'yellow', 'blue-dark', 'cyan', 'purple',
-    // 'violet', 'pink', 'green-dark', 'green', 'green-light', 'black', or 'white'
-    markerColor: issue.status === "closed" ? "orange-dark" : "cyan",
-    //'circle', 'square', 'star', or 'penta'
-    shape: "circle",
-    prefix: ""
-  };
-
-  return Leaflet.ExtraMarkers.icon({
-    ...defaulIconProps,
-    ...opts
-  });
+  let cls = getIconClassForIssueType(
+    issue_type,
+    "toucan-div-icon-marker marker-"
+  );
+  return divIcon({ className: cls });
 }
 
 export default getMarkerForIssue;
