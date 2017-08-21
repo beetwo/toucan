59d20fdddc127eae9897f9be5e01df215b4f5ee7
Sean Mc Allister <sean@gmx.org>
2017-08-21 22:47:55 +0200

    fix invitations and enlarge icons

--- toucan/invitations/invites.py
+++ toucan/invitations/invites.py
@@ -7,18 +7,18 @@ from django.template.context import RequestContext
 from .settings import INVITATION_VALID_DAYS
 
 
-def send_invitation_mail(invitation, ctx={}):
+def send_invitation_mail(invitation, request=None):
     subject = _('You have been invited to join us on Toucan')
 
     context = {
         'invitation': invitation,
         'invitation_valid_days': INVITATION_VALID_DAYS,
-        'invite_url': invitation.get_verification_link(request=ctx.request if hasattr(ctx, 'request') else None),
+        'invite_url': invitation.get_verification_link(request=request),
         'email': invitation.email,
         'organisation_name': invitation.organisation.name
     }
-    ctx.update(context)
-    message = render_to_string('invitations/email/invitation.txt', ctx)
+
+    message = render_to_string('invitations/email/invitation.txt', context, request=request)
     send_mail(
         subject,
         message,
@@ -29,5 +29,5 @@ def send_invitation_mail(invitation, ctx={}):
 def send_invitation_with_request_context(invitation, request):
     return send_invitation_mail(
         invitation,
-        RequestContext(request, {})
+        request
     )
