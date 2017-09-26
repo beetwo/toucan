f4035150edd0de112ba2a4ee46847123998db974
Sean Mc Allister <sean@gmx.org>
2017-09-26 19:29:06 +0200

    missed a draft-js import

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,71 +1,44 @@
-import isString from 'lodash/isString'
-import { Entity } from 'draft-js'
-
+import isString from "lodash/isString";
 
 export default function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  var cookieValue = null;
+  if (document.cookie && document.cookie != "") {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) == name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 
-export var csrftoken = getCookie('csrftoken');
+export var csrftoken = getCookie("csrftoken");
 
-
-function getHeaders () {
+function getHeaders() {
   return {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'X-CSRFToken': getCookie('csrftoken')
-  }
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "X-CSRFToken": getCookie("csrftoken")
+  };
 }
 
 export function jsonPost(url, data) {
-  return fetch(
-    url,
-    {
-        method: 'post',
-        credentials: 'same-origin',
-        headers: getHeaders(),
-        body: isString(data) ? data : JSON.stringify(data)
-      }
-    )
+  return fetch(url, {
+    method: "post",
+    credentials: "same-origin",
+    headers: getHeaders(),
+    body: isString(data) ? data : JSON.stringify(data)
+  });
 }
-
 
 export function jsonGet(url) {
-  return fetch(
-    url,
-    {
-      method: 'get',
-      credentials: 'same-origin',
-      headers: getHeaders()
-    }
-  )
-}
-
-export function convertDraft(contentState) {
-  console.log(contentState)
-  let blocks = contentState.getBlockMap().toArray()
-  blocks.forEach((b) => {
-    console.log('new block');
-    b.findEntityRanges(
-      (x) => {
-        let e = x.getEntity();
-        return true
-      },
-      (start, end) => { console.log(start, end) }
-    );
+  return fetch(url, {
+    method: "get",
+    credentials: "same-origin",
+    headers: getHeaders()
   });
-  return contentState.getPlainText();
-
 }
