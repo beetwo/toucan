a34da3748a6723801170aa1f93e0f67844cee400
Sean Mc Allister <sean@gmx.org>
2017-09-28 23:25:51 +0200

    contact form on help page

--- toucan/help/urls.py
+++ toucan/help/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-
+from .views import HelpPage
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='help/start.html'), name='start'),
+    url(r'^$', HelpPage.as_view(), name='start'),
     url(r'^initial_login/$', TemplateView.as_view(template_name='help/first_steps.html'), name='first_steps'),
 ]
--- toucan/help/views.py
+++ toucan/help/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from contact_form.views import ContactFormView
+from braces.views import FormValidMessageMixin
+
+class HelpPage(FormValidMessageMixin, ContactFormView):
+
+    template_name = 'help/start.html'
+
+    form_valid_message = 'Your message has been sent.'
+
+    def get_initial(self):
+        if self.request.user.is_authenticated:
+            user = self.request.user
+            return {
+                'email': user.email,
+                'name': user.username
+            }
+        return {}
+
+    def get_success_url(self):
+        return reverse('help:start')
