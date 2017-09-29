e36d013ee6c279fb66e8e855d855195fb748647c
Sean Mc Allister <sean.mcallister@apa.at>
2017-09-29 11:42:53 +0200

    center the image map to point

--- toucan/utils/staticmap.py
+++ toucan/utils/staticmap.py
@@ -5,12 +5,13 @@ def get_url_for_geographic_notification(ns):
     token = getattr(settings, 'MAPBOX_TOKEN', False)
     if not token:
         return ''
-    # print(ns)
+
+    center = ns.point.coords
     url = "https://api.mapbox.com/styles/v1/mapbox/light-v9/static/{lon},{lat},12/{x}x{y}?access_token={token}"
     return url.format(**{
         "token": token,
-        "lon": -122.4241,
-        "lat": 37.78,
+        "lon": center[0],
+        "lat": center[1],
         "x": 138,
         "y": 138
     })
