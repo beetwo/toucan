2e67e7a81b364b15d0f4fd1cca6bb4d20c0d69d1
Sean Mc Allister <sean.mcallister@apa.at>
2017-09-29 12:21:25 +0200

    remove french ;-)

--- frontend/js/components/fileUploader.js
+++ frontend/js/components/fileUploader.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React from "react";
 
-export function ProgressBar({progress}) {
+export function ProgressBar({ progress }) {
   progress = progress || 50;
-  let s = {'width': progress + '%'};
-  return (<div className="progress">
-    <div className="progress-bar" style={s} />
-  </div>)
+  let s = { width: progress + "%" };
+  return (
+    <div className="progress">
+      <div className="progress-bar" style={s} />
+    </div>
+  );
 }
 
-export default function ImageUploader({preview, filename, progress, finished}) {
-  return <div className='image-upload'>
-      {
-        preview ?
-        <img src={preview} alt='file preview' />
-        : null
-      }
-      {
-        finished ?
-         null
-         : <ProgressBar progress={progress || 0} />
-      }
+export default function ImageUploader({
+  preview,
+  filename,
+  progress,
+  finished
+}) {
+  return (
+    <div className="image-upload">
+      {preview ? <img src={preview} alt="file preview" /> : null}
+      {finished ? null : <ProgressBar progress={progress || 0} />}
       <br />
-      <span className='attachement-name'>{filename}</span>
-  </div>;
+      <span className="attachment-name">{filename}</span>
+    </div>
+  );
 }
