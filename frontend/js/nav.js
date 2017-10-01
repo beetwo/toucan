8f528aa8f887f68a5b96299c42bf1da0f1bb8e7b
Sean Mc Allister <sean@gmx.org>
2017-10-01 12:26:21 +0200

    pure css nav

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -1,86 +1,109 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-import { NavBar } from "simple-react-bootstrap";
 import { connect } from "react-redux";
 import cn from "classnames";
 
 const ToucanLogo = require("../assets/toucan_logo.png");
 
-const Nav = ({ location, current_user }) => {
-  const user = current_user.user || {};
-  let active =
-    (location.pathname.split("/")[1] === "orgs" && "orgs") || "needs";
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
 
-  const links = current_user.links || [];
-  const keyed_links = links.reduce(
-    (prev, link) => ({ ...prev, [link.key]: link }),
-    {}
-  );
-  // the actual links
-  const settings_link = keyed_links.settings;
-  const logout_link = keyed_links.logout;
-  const support_link = keyed_links.support;
+  toggle() {
+    this.setState(state => ({ open: !state.open }));
+  }
 
-  return (
-    <NavBar>
-      <NavBar.Header>
-        <NavBar.Brand>
-          <Link to="/">
-            <img
-              alt="Toucan Logo"
-              className="navbar-logo img-responsive"
-              src={ToucanLogo}
-            />
-          </Link>
-        </NavBar.Brand>
-        <NavBar.Toggle />
-      </NavBar.Header>
+  render() {
+    const { location, current_user } = this.props;
+    let active =
+      (location.pathname.split("/")[1] === "orgs" && "orgs") || "needs";
 
-      <NavBar.Nav className="navbar-main">
-        <li className={cn("nav-issues", { active: active === "needs" })}>
-          <Link to="/">Needs</Link>
-        </li>
-        <li className={cn("nav-orgs", { active: active === "orgs" })}>
-          <Link to="/orgs/">Organisations</Link>
-        </li>
-        {/* this one comes from the server and might not be available at first render*/}
-        {settings_link ? (
-          <li className="nav-settings">
-            <a href={settings_link.url}>Profile and Settings</a>
-          </li>
-        ) : null}
-      </NavBar.Nav>
-      <NavBar.Nav className="navbar-secondary navbar-right">
-        {support_link ? (
-          <NavBar.Item
-            className="nav-muted"
-            key={support_link.url}
-            href={support_link.url}
+    const links = current_user.links || [];
+
+    const keyed_links = links.reduce(
+      (prev, link) => ({ ...prev, [link.key]: link }),
+      {}
+    );
+    // the actual links
+    const settings_link = keyed_links.settings;
+    const logout_link = keyed_links.logout;
+    const support_link = keyed_links.support;
+
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button
+              type="button"
+              onClick={this.toggle}
+              className={cn("navbar-toggle", { collapsed: this.state.open })}
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+            </button>
+            <Link to="/" className="navbar-brand">
+              <img
+                alt="Toucan Logo"
+                className="navbar-logo img-responsive"
+                src={ToucanLogo}
+              />
+            </Link>
+          </div>
+          <div
+            className={cn("collapse navbar-collapse", { in: this.state.open })}
+            onClick={this.toggle}
           >
-            <span className="icon icon-support icon-lg" />
-            Support
-          </NavBar.Item>
-        ) : null}
-        {logout_link ? (
-          <NavBar.Item
-            className="nav-muted"
-            key={logout_link.url}
-            href={logout_link.url}
-          >
-            <span className="icon icon-logout icon-lg" />
-            Logout
-          </NavBar.Item>
-        ) : null}
-      </NavBar.Nav>
-    </NavBar>
-  );
-};
+            <ul className="nav navbar-nav navbar-main">
+              <li className={cn("nav-issues", { active: active === "needs" })}>
+                <Link to="/">Needs</Link>
+              </li>
+              <li className={cn("nav-orgs", { active: active === "orgs" })}>
+                <Link to="/orgs/">Organisations</Link>
+              </li>
+              {/* this one comes from the server and might not be available at first render*/}
+              {settings_link ? (
+                <li className="nav-settings">
+                  <a href={settings_link.url}>Profile and Settings</a>
+                </li>
+              ) : null}
+            </ul>
+            <ul className="nav navbar-nav navbar-right navbar-secondary">
+              {support_link ? (
+                <li className="nav-muted">
+                  <a key={support_link.url} href={support_link.url}>
+                    <span className="icon icon-support icon-lg" />
+                    Support
+                  </a>
+                </li>
+              ) : null}
+              {logout_link ? (
+                <li className="nav-muted" key={logout_link.url}>
+                  <a href={logout_link.url}>
+                    <span className="icon icon-logout icon-lg" />
+                    Logout
+                  </a>
+                </li>
+              ) : null}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
 
 const ToucanNav = connect((state, { location }) => {
   return {
     location,
     current_user: state.currentUser
   };
-})(Nav);
+})(NavBar);
 
 export default withRouter(ToucanNav);
